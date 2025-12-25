e8dce2c1d4c35f35a1c8771b491bc105d292fa8d
antonio10bc-hub <antonioasis10bc@gmail.com>
2025-12-25 23:35:01 +0100

    ajuste error tonto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-// import { Inter } from 'next/font/google' // O la fuente que uses
 import { clsx } from 'clsx'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Magic Poker',
@@ -16,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es" className="h-full"> {/* Asegura altura completa */}
-      <body className={clsx("h-full m-0", /* inter.className */)}> {/* Altura completa y sin margen */}
+    <html lang="es" className="h-full">
+      <body className={clsx("h-full m-0")}>
         {children}
       </body>
     </html>
